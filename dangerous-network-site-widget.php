339d32ad6542de29005f91e6592056d8cbbf36be
Ryan Imel <ryanimel@Ryan-Imels-MacBook-Air.local>
2012-08-27 17:53:07 -0400

    First test of the site query.

--- dangerous-network-site-widget.php
+++ dangerous-network-site-widget.php
@@ -17,3 +17,44 @@ function rwi_dnsw_query() {
 
 
 // Build out the widget.
+// Kudos http://codex.wordpress.org/Function_Reference/register_widget
+class DangerousNetworkSiteWidget extends WP_Widget {
+
+	function DangerousNetworkSiteWidget() {
+		// Instantiate the parent object
+		parent::__construct( false, 'Network Sites' );
+	}
+
+	// Widget output
+	function widget( $args, $instance ) {
+		
+		$network_sites = get_blog_list( 0, 'all' );
+		
+		$output .= '<ul>';
+		
+		foreach( $network_sites as $network_site ) {
+			
+			$output .= '<li>Blog ' . $network_site['blog_id'] . ': ' . $network_site['domain'] . $network_site['path'] . '</li>';
+			
+		}
+		
+		$output .= '</ul>';
+		
+		echo $output;
+		
+	}
+
+	function update( $new_instance, $old_instance ) {
+		// Save widget options
+	}
+
+	function form( $instance ) {
+		// Output admin widget options form
+	}
+}
+
+function rwi_dnsw__register_widgets() {
+	register_widget( 'DangerousNetworkSiteWidget' );
+}
+
+add_action( 'widgets_init', 'rwi_dnsw__register_widgets' );
